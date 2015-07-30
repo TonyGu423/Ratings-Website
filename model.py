5956831ff54ee5cb6bb575a92253ce5cb6beaf74
Hackbright Student <info@hackbrightacademy.com>
2015-07-30 12:59:05 -0700

    Added pearson similarity method on User class

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 """Models and database functions for Ratings project."""
 
 from flask_sqlalchemy import SQLAlchemy
+import correlation
 
 # This is the connection to the SQLite database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
@@ -27,6 +28,26 @@ class User(db.Model):
         """Provide helpful information representation when printed!"""
 
         return "<User user_id = %s email = %s>" %(self.user_id, self.email)
+
+    def similarity(self, other):
+        """Return Pearson rating for user compared to other user."""
+
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.score, r.score) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+
+        else:
+            return 0.0
 
 class Movie(db.Model):
     """Movie in the database"""
